e0ac9cb18041d4eb03051803013b5b0cd6f4364b
LCY <GuoBin@mac-mini.local>
2016-05-04 12:54:43 +0800

    Initial Commit

---
+++ UIBezierPathDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIBezierPathDemo
+//
+//  Created by 薛国宾 on 16/5/4.
+//  Copyright © 2016年 千里之行始于足下. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIBezierPathDemoTests/UIBezierPathDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  UIBezierPathDemoTests.m
+//  UIBezierPathDemoTests
+//
+//  Created by 薛国宾 on 16/5/4.
+//  Copyright © 2016年 千里之行始于足下. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIBezierPathDemoTests : XCTestCase
+
+@end
+
+@implementation UIBezierPathDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
